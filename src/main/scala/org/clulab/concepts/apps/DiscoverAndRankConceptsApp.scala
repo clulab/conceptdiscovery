6644c8c63ae0397cc84f12bd84a6d64a811351d1
Zheng Tang <oscartang1120@gmail.com>
2021-05-21 00:29:02 -0700

    Update DiscoverAndRankConceptsApp.scala

--- src/main/scala/org/clulab/concepts/apps/DiscoverAndRankConceptsApp.scala
+++ src/main/scala/org/clulab/concepts/apps/DiscoverAndRankConceptsApp.scala
@@ -2,8 +2,12 @@ package org.clulab.concepts.apps
 
 import org.clulab.concepts.{ConceptDiscoverer, ConceptSink, ConceptSource, DiscoveryDocument}
 import org.clulab.utils.FileUtils
+import java.util.Calendar
+import java.util.concurrent.TimeUnit
+import java.io.{FileOutputStream, PrintStream}
 
 object DiscoverAndRankConceptsApp extends App {
+  val start = Calendar.getInstance
   val inputDir = args(0)
   val thresholdFrequency = args(1).toDouble
   val thresholdSimilarity = args(2).toDouble
@@ -24,9 +28,18 @@ object DiscoverAndRankConceptsApp extends App {
     else
       None
   }
+  val time = Calendar.getInstance
+  println(TimeUnit.MILLISECONDS.toSeconds(time.getTimeInMillis() - start.getTimeInMillis()))
   val concepts = conceptDiscovery.discoverMostFrequentConcepts(discoveryDocuments, sentenceThresholdOpt, thresholdFrequency, topPick)
+  val time2 = Calendar.getInstance
+  println(TimeUnit.MILLISECONDS.toSeconds(time2.getTimeInMillis() - time.getTimeInMillis()))
+  println(concepts.size)
   val rankedConcepts = conceptDiscovery.rankConcepts(concepts, thresholdSimilarity)
+  val time3 = Calendar.getInstance
+  println(TimeUnit.MILLISECONDS.toSeconds(time3.getTimeInMillis() - time2.getTimeInMillis()))
   val conceptSink = new ConceptSink(rankedConcepts)
 
-  conceptSink.printJson()
+  Console.withOut(new PrintStream(new FileOutputStream("output_full.json"))){
+    conceptSink.printJson()
+  }
 }
